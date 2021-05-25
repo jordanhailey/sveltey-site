0e7ec68e50fa1beaa257493d70332628eaccf4f1
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-05-25 12:22:46 -0700

    confirming __tests__ folder is working as expected

--- src/__tests__/sum.test.js
+++ src/__tests__/sum.test.js
@@ -1,4 +1,4 @@
-import {sum} from './sum';
+import {sum} from '../sum';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -6,3 +6,4 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 2 to not be 4', () => {
   expect(sum(1, 2)).not.toBe(4)
 });
+
